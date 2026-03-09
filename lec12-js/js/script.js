d99182fd82567f567b551f8b0ddb80cbc01b9600
Mohammed Naji <m.naji1512@gmail.com>
2026-03-09 14:20:51 +0200

    js loops

---
+++ lec12-js/js/script.js
@@ -0,0 +1,79 @@
+// console.log(myname);
+// var myname = "Mohammed";
+// console.log(myname);
+
+// var age = 20;
+// age = 50;
+// console.log(age);
+
+// let myname = "Mohammed";
+// myname = "Ali";
+// console.log(myname);
+
+// const myname = "Mohammed";
+// myname = "dd";
+// console.log(myname);
+
+// const age = 20;
+
+// let name = [10, 20];
+// console.log(typeof name);
+
+// let course = {
+//   name: "Front End",
+//   hours: 40,
+//   instructors: "Mohammed Naji",
+// };
+
+// console.log(course.name);
+
+// let foods = ["Shawerma", "Kebab", "Makloopa"];
+
+// console.log(foods);
+
+// let courses = [
+//   {
+//     name: "PHP",
+//     duration: 40,
+//   },
+//   {
+//     name: "Python",
+//     duration: 40,
+//   },
+//   {
+//     name: "Js",
+//     duration: 40,
+//   },
+//   {
+//     name: "Front",
+//     duration: 40,
+//   },
+// ];
+
+// console.log(courses);
+
+// let course = {
+//   name: "Front End",
+//   hours: 40,
+//   instructors: "Mohammed Naji",
+// };
+
+// for
+// for (const key in course) {
+//   console.log(key);
+// }
+
+// for (const element of course) {
+//   console.log(element);
+// }
+let foods = ["Shawerma", "Kebab", "Makloopa"];
+for (let i = 0; i < foods.length; i++) {
+  console.log(foods[i]);
+}
+// while
+let i = 0;
+while(i < foods.length) {
+  
+}
+// do while
+// foreach
